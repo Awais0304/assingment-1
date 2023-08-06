a0524fa30095fe60458bc56d2b4ca640b2a1391e
Muhammad Awais Asim <112088956+Awais0304@users.noreply.github.com>
2023-08-05 18:18:21 -0700

    Add files via upload

---
+++ assignment-1/task-1.js
@@ -0,0 +1,30 @@
+console.log("Hey Abu Hurairah, I have started learning typescript");
+// store your name in a variable
+var myname = "Muhammad Awais Asim";
+console.log(myname);
+// store 10 numbers in variable
+var num1 = 22;
+var num2 = 25;
+var num3 = 26;
+var num4 = 29;
+var num5 = 21;
+var num6 = 23;
+var num7 = 30;
+var num8 = 24;
+var num9 = 27;
+var num10 = 28;
+// now add all the number 
+var sum = (num1 + num2 + num3 + num4 + num5 + num6 + num7 + num8 + num9 + num10);
+console.log("sum is=" + sum);
+// subtract alll the number
+var sub = (num1 - num2 - num3 - num4 - num5 - num6 - num7 - num8 - num9 - num10);
+console.log("sub is= " + sub);
+// multiplying all the numbers
+var multi = (num1 * num2 * num3 * num4 * num5 * num6 * num7 * num8 * num9 * num10);
+console.log("multiply is= " + multi);
+// divide any two numbers each of them
+var divide = (num2 / num1);
+console.log("division is=" + divide);
+// now, perform all opertion at  same time
+var combine = (num1 + num2 - num3 * num4 / num5);
+console.log("all opertion result is=" + combine);
---
+++ assignment-1/task-1.ts
@@ -0,0 +1,49 @@
+console.log("Hey Abu Hurairah, I have started learning typescript");
+
+// store your name in a variable
+
+var myname: string = "Muhammad Awais Asim";
+console.log(myname);
+
+// store 10 numbers in variable
+
+var num1: number = 22;
+
+var num2: number = 25;
+
+var num3: number = 26;
+
+var num4: number = 29;
+
+var num5: number = 21;
+
+var num6: number = 23;
+
+var num7: number = 30;
+
+var num8: number = 24;
+
+var num9: number = 27;
+
+var num10: number = 28;
+
+// now add all the number 
+
+var sum: number = (num1 + num2 + num3 + num4 + num5 + num6 + num7 + num8 + num9 + num10);
+console.log("sum is=" + sum);
+
+// subtract alll the number
+var sub: number = (num1 - num2 - num3 - num4 - num5 - num6 - num7 - num8 - num9 - num10);
+console.log("sub is= " + sub);
+
+// multiplying all the numbers
+var multi: number = (num1 * num2 * num3 * num4 * num5 * num6 * num7 * num8 * num9 * num10);
+console.log("multiply is= " + multi);
+
+// divide any two numbers each of them
+var divide: number = (num2 / num1);
+console.log("division is=" + divide);
+
+// now, perform all opertion at  same time
+var combine:number=(num1 +num2 - num3 * num4 / num5);
+console.log("all opertion result is=" + combine);
